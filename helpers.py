12bcda06b49ec0f8a8bbc00d3711bb66b4eb49de
weihan1 <weihan.luo@mail.utoronto.ca>
2025-03-17 12:57:06 -0400

    helpers.py

--- helpers.py
+++ helpers.py
@@ -1,265 +1,209 @@
-
-"""
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# This software is free for non-commercial, research and evaluation use
-# under the terms of the LICENSE.md file found here:
-# https://github.com/graphdeco-inria/gaussian-splatting/blob/main/LICENSE.md
-#
-# For inquiries contact  george.drettakis@inria.fr
-
-#######################################################################################################################
-##### NOTE: CODE IN THIS FILE IS NOT INCLUDED IN THE OVERALL PROJECT'S MIT LICENSE #####
-##### USE OF THIS CODE FOLLOWS THE COPYRIGHT NOTICE ABOVE #####
-#######################################################################################################################
-"""
-
 import torch
-import torch.nn.functional as func
-from torch.autograd import Variable
-from math import exp
-import numpy as np
-from plyfile import PlyData, PlyElement
 import os
-import matplotlib.pyplot as plt
+import open3d as o3d
+import numpy as np
+from diff_gaussian_rasterization import GaussianRasterizationSettings as Camera
+from typing import NamedTuple
+from plyfile import PlyData, PlyElement
+import math
+from sklearn.neighbors import NearestNeighbors
 
-def build_rotation(q):
-    norm = torch.sqrt(q[:, 0] * q[:, 0] + q[:, 1] * q[:, 1] + q[:, 2] * q[:, 2] + q[:, 3] * q[:, 3])
-    q = q / norm[:, None]
-    rot = torch.zeros((q.size(0), 3, 3), device='cuda')
-    r = q[:, 0]
-    x = q[:, 1]
-    y = q[:, 2]
-    z = q[:, 3]
-    rot[:, 0, 0] = 1 - 2 * (y * y + z * z)
-    rot[:, 0, 1] = 2 * (x * y - r * z)
-    rot[:, 0, 2] = 2 * (x * z + r * y)
-    rot[:, 1, 0] = 2 * (x * y + r * z)
-    rot[:, 1, 1] = 1 - 2 * (x * x + z * z)
-    rot[:, 1, 2] = 2 * (y * z - r * x)
-    rot[:, 2, 0] = 2 * (x * z - r * y)
-    rot[:, 2, 1] = 2 * (y * z + r * x)
-    rot[:, 2, 2] = 1 - 2 * (x * x + y * y)
-    return rot
+def setup_camera(w, h, k, w2c, near=0.01, far=100):
+    fx, fy, cx, cy = k[0][0], k[1][1], k[0][2], k[1][2]
+    w2c = torch.tensor(w2c).cuda().float()
+    cam_center = torch.inverse(w2c)[:3, 3]
+    w2c = w2c.unsqueeze(0).transpose(1, 2)
+    opengl_proj = torch.tensor([[2 * fx / w, 0.0, -(w - 2 * cx) / w, 0.0],
+                                [0.0, 2 * fy / h, -(h - 2 * cy) / h, 0.0],
+                                [0.0, 0.0, far / (far - near), -(far * near) / (far - near)],
+                                [0.0, 0.0, 1.0, 0.0]]).cuda().float().unsqueeze(0).transpose(1, 2)
+    full_proj = w2c.bmm(opengl_proj)
+    cam = Camera(
+        image_height=h,
+        image_width=w,
+        tanfovx=w / (2 * fx),
+        tanfovy=h / (2 * fy),
+        bg=torch.tensor([0, 0, 0], dtype=torch.float32, device="cuda"),
+        scale_modifier=1.0,
+        viewmatrix=w2c,
+        projmatrix=full_proj,
+        sh_degree=0,
+        campos=cam_center,
+        prefiltered=False
+    )
+    return cam
+
+def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
+    Rt = np.zeros((4, 4))
+    Rt[:3, :3] = R.transpose()
+    Rt[:3, 3] = t
+    Rt[3, 3] = 1.0
+
+    C2W = np.linalg.inv(Rt)
+    cam_center = C2W[:3, 3]
+    cam_center = (cam_center + translate) * scale
+    C2W[:3, 3] = cam_center
+    Rt = np.linalg.inv(C2W)
+    return np.float32(Rt)
+
+def getProjectionMatrix(znear, zfar, fovX, fovY):
+    tanHalfFovY = math.tan((fovY / 2))
+    tanHalfFovX = math.tan((fovX / 2))
+
+    top = tanHalfFovY * znear
+    bottom = -top
+    right = tanHalfFovX * znear
+    left = -right
+
+    P = torch.zeros(4, 4)
+
+    z_sign = 1.0
+
+    P[0, 0] = 2.0 * znear / (right - left)
+    P[1, 1] = 2.0 * znear / (top - bottom)
+    P[0, 2] = (right + left) / (right - left)
+    P[1, 2] = (top + bottom) / (top - bottom)
+    P[3, 2] = z_sign
+    P[2, 2] = z_sign * zfar / (zfar - znear)
+    P[2, 3] = -(zfar * znear) / (zfar - znear)
+    return P
 
 
-def calc_mse(img1, img2):
-    return ((img1 - img2) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
+def setup_camera_blender(w, h, R, T, FoVx, FoVy, trans=np.array([0.0, 0.0, 0.0]), scale=1.0, zfar=100, znear=0.01):
+    """
+    Return the camera from blender dataset.
+    """
+    world_view_transform = torch.tensor(getWorld2View2(R, T, trans, scale)).transpose(0, 1)
+    projection_matrix = getProjectionMatrix(znear=znear, zfar=zfar, fovX=FoVx, fovY=FoVy).transpose(0,1)
+    full_proj_transform = (world_view_transform.unsqueeze(0).bmm(projection_matrix.unsqueeze(0))).squeeze(0)
+    camera_center = world_view_transform.inverse()[3, :3]
+    tanfovx = math.tan(FoVx * 0.5)
+    tanfovy = math.tan(FoVy * 0.5)
+    camera = Camera(image_height=h,
+                    image_width=w,
+                    tanfovx=tanfovx,
+                    tanfovy=tanfovy,
+                    bg=torch.tensor([0,0,0], dtype=torch.float32, device="cuda"),
+                    scale_modifier=1.0,
+                    viewmatrix=world_view_transform.cuda(),
+                    projmatrix=full_proj_transform.cuda(),
+                    sh_degree=0,
+                    campos=camera_center,
+                    prefiltered=False)
+    return camera
+    
 
-
-def calc_psnr(img1, img2):
-    mse = ((img1 - img2) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
-    return 20 * torch.log10(1.0 / torch.sqrt(mse))
-
-
-def gaussian(window_size, sigma):
-    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
-    return gauss / gauss.sum()
-
-
-def create_window(window_size, channel):
-    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
-    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
-    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
-    return window
-
-
-def calc_ssim(img1, img2, window_size=11, size_average=True):
-    channel = img1.size(-3)
-    window = create_window(window_size, channel)
-
-    if img1.is_cuda:
-        window = window.cuda(img1.get_device())
-    window = window.type_as(img1)
-
-    return _ssim(img1, img2, window, window_size, channel, size_average)
-
-
-def _ssim(img1, img2, window, window_size, channel, size_average=True):
-    mu1 = func.conv2d(img1, window, padding=window_size // 2, groups=channel)
-    mu2 = func.conv2d(img2, window, padding=window_size // 2, groups=channel)
-
-    mu1_sq = mu1.pow(2)
-    mu2_sq = mu2.pow(2)
-    mu1_mu2 = mu1 * mu2
-
-    sigma1_sq = func.conv2d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
-    sigma2_sq = func.conv2d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
-    sigma12 = func.conv2d(img1 * img2, window, padding=window_size // 2, groups=channel) - mu1_mu2
-
-    c1 = 0.01 ** 2
-    c2 = 0.03 ** 2
-
-    ssim_map = ((2 * mu1_mu2 + c1) * (2 * sigma12 + c2)) / ((mu1_sq + mu2_sq + c1) * (sigma1_sq + sigma2_sq + c2))
-
-    if size_average:
-        return ssim_map.mean()
+def PILtoTorch(pil_image, resolution):
+    if resolution is not None:
+        resized_image_PIL = pil_image.resize(resolution)
     else:
-        return ssim_map.mean(1).mean(1).mean(1)
+        resized_image_PIL = pil_image
+    if np.array(resized_image_PIL).max()!=1:
+        resized_image = torch.from_numpy(np.array(resized_image_PIL)) / 255.0
+    else:
+        resized_image = torch.from_numpy(np.array(resized_image_PIL))
+    if len(resized_image.shape) == 3:
+        return resized_image.permute(2, 0, 1)
+    else:
+        return resized_image.unsqueeze(dim=-1).permute(2, 0, 1)
+
+def fov2focal(fov, pixels):
+    return pixels / (2 * math.tan(fov / 2))
+
+def focal2fov(focal, pixels):
+    return 2*math.atan(pixels/(2*focal))
+
+def params2rendervar(params):
+    rendervar = {
+        'means3D': params['means3D'],
+        'colors_precomp': params['rgb_colors'],
+        'rotations': torch.nn.functional.normalize(params['unnorm_rotations']),
+        'opacities': torch.sigmoid(params['logit_opacities']),
+        'scales': torch.exp(params['log_scales']),
+        'means2D': torch.zeros_like(params['means3D'], requires_grad=True, device="cuda") + 0
+    }
+    return rendervar
 
 
-def accumulate_mean2d_gradient(variables):
-    variables['means2D_gradient_accum'][variables['seen']] += torch.norm(
-        variables['means2D'].grad[variables['seen'], :2], dim=-1)
-    variables['denom'][variables['seen']] += 1
-    return variables
+def l1_loss_v1(x, y):
+    return torch.abs((x - y)).mean()
 
 
-def update_params_and_optimizer(new_params, params, optimizer):
-    for k, v in new_params.items():
-        group = [x for x in optimizer.param_groups if x["name"] == k][0]
-        stored_state = optimizer.state.get(group['params'][0], None)
-
-        stored_state["exp_avg"] = torch.zeros_like(v)
-        stored_state["exp_avg_sq"] = torch.zeros_like(v)
-        del optimizer.state[group['params'][0]]
-
-        group["params"][0] = torch.nn.Parameter(v.requires_grad_(True))
-        optimizer.state[group['params'][0]] = stored_state
-        params[k] = group["params"][0]
-    return params
+def l1_loss_v2(x, y):
+    return (torch.abs(x - y).sum(-1)).mean()
 
 
-def cat_params_to_optimizer(new_params, params, optimizer):
-    for k, v in new_params.items():
-        group = [g for g in optimizer.param_groups if g['name'] == k][0]
-        stored_state = optimizer.state.get(group['params'][0], None)
-        if stored_state is not None:
-            stored_state["exp_avg"] = torch.cat((stored_state["exp_avg"], torch.zeros_like(v)), dim=0)
-            stored_state["exp_avg_sq"] = torch.cat((stored_state["exp_avg_sq"], torch.zeros_like(v)), dim=0)
-            del optimizer.state[group['params'][0]]
-            group["params"][0] = torch.nn.Parameter(torch.cat((group["params"][0], v), dim=0).requires_grad_(True))
-            optimizer.state[group['params'][0]] = stored_state
-            params[k] = group["params"][0]
+def weighted_l2_loss_v1(x, y, w):
+    return torch.sqrt(((x - y) ** 2) * w + 1e-20).mean()
+
+
+def weighted_l2_loss_v2(x, y, w):
+    return torch.sqrt(((x - y) ** 2).sum(-1) * w + 1e-20).mean()
+
+
+def quat_mult(q1, q2):
+    w1, x1, y1, z1 = q1.T
+    w2, x2, y2, z2 = q2.T
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+    z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+    return torch.stack([w, x, y, z]).T
+
+
+def o3d_knn(pts, num_knn):
+    indices = []
+    sq_dists = []
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(np.ascontiguousarray(pts, np.float64))
+    pcd_tree = o3d.geometry.KDTreeFlann(pcd)
+    for p in pcd.points:
+        [_, i, d] = pcd_tree.search_knn_vector_3d(p, num_knn + 1)
+        indices.append(i[1:])
+        sq_dists.append(d[1:])
+    return np.array(sq_dists), np.array(indices)
+
+def knn(x, K= 4):
+    """
+    Return K closest neighbor distances from x.
+    """
+    x_np = x.cpu().numpy()
+    model = NearestNeighbors(n_neighbors=K, metric="euclidean").fit(x_np)
+    distances, _ = model.kneighbors(x_np)
+    return torch.from_numpy(distances).to(x)
+
+def params2cpu(params, is_initial_timestep):
+    if is_initial_timestep:
+        res = {k: v.detach().cpu().contiguous().numpy() for k, v in params.items()}
+    else:
+        res = {k: v.detach().cpu().contiguous().numpy() for k, v in params.items() if
+               k in ['means3D', 'rgb_colors', 'unnorm_rotations']}
+    return res
+
+
+def save_params(output_params, seq, exp):
+    to_save = {}
+    for k in output_params[0].keys():
+        if k in output_params[1].keys():
+            to_save[k] = np.stack([params[k] for params in output_params])
         else:
-            group["params"][0] = torch.nn.Parameter(torch.cat((group["params"][0], v), dim=0).requires_grad_(True))
-            params[k] = group["params"][0]
-    return params
+            to_save[k] = output_params[0][k]
+    os.makedirs(f"./output/{exp}/{seq}", exist_ok=True)
+    np.savez(f"./output/{exp}/{seq}/params", **to_save)
 
+class BasicPointCloud(NamedTuple):
+    points : np.array
+    colors : np.array
+    normals : np.array
+    seg_colors: np.array
 
-def remove_points(to_remove, params, variables, optimizer):
-    to_keep = ~to_remove
-    keys = [k for k in params.keys() if k not in ['cam_m', 'cam_c']]
-    for k in keys:
-        group = [g for g in optimizer.param_groups if g['name'] == k][0]
-        stored_state = optimizer.state.get(group['params'][0], None)
-        if stored_state is not None:
-            stored_state["exp_avg"] = stored_state["exp_avg"][to_keep]
-            stored_state["exp_avg_sq"] = stored_state["exp_avg_sq"][to_keep]
-            del optimizer.state[group['params'][0]]
-            group["params"][0] = torch.nn.Parameter((group["params"][0][to_keep].requires_grad_(True)))
-            optimizer.state[group['params'][0]] = stored_state
-            params[k] = group["params"][0]
-        else:
-            group["params"][0] = torch.nn.Parameter(group["params"][0][to_keep].requires_grad_(True))
-            params[k] = group["params"][0]
-    variables['means2D_gradient_accum'] = variables['means2D_gradient_accum'][to_keep]
-    variables['denom'] = variables['denom'][to_keep]
-    variables['max_2D_radius'] = variables['max_2D_radius'][to_keep]
-    return params, variables
-
-
-def inverse_sigmoid(x):
-    return torch.log(x / (1 - x))
-
-
-def densify(params, variables, optimizer, i):
-    if i <= 5000:
-        variables = accumulate_mean2d_gradient(variables)
-        grad_thresh = 0.0002
-        if (i >= 500) and (i % 100 == 0):
-            grads = variables['means2D_gradient_accum'] / variables['denom']
-            grads[grads.isnan()] = 0.0
-            to_clone = torch.logical_and(grads >= grad_thresh, (
-                        torch.max(torch.exp(params['log_scales']), dim=1).values <= 0.01 * variables['scene_radius']))
-            new_params = {k: v[to_clone] for k, v in params.items() if k not in ['cam_m', 'cam_c']}
-            params = cat_params_to_optimizer(new_params, params, optimizer)
-            num_pts = params['means3D'].shape[0]
-
-            padded_grad = torch.zeros(num_pts, device="cuda")
-            padded_grad[:grads.shape[0]] = grads
-            to_split = torch.logical_and(padded_grad >= grad_thresh,
-                                         torch.max(torch.exp(params['log_scales']), dim=1).values > 0.01 * variables[
-                                             'scene_radius'])
-            n = 2  # number to split into
-            new_params = {k: v[to_split].repeat(n, 1) for k, v in params.items() if k not in ['cam_m', 'cam_c']}
-            stds = torch.exp(params['log_scales'])[to_split].repeat(n, 1)
-            means = torch.zeros((stds.size(0), 3), device="cuda")
-            samples = torch.normal(mean=means, std=stds)
-            rots = build_rotation(params['unnorm_rotations'][to_split]).repeat(n, 1, 1)
-            new_params['means3D'] += torch.bmm(rots, samples.unsqueeze(-1)).squeeze(-1)
-            new_params['log_scales'] = torch.log(torch.exp(new_params['log_scales']) / (0.8 * n))
-            params = cat_params_to_optimizer(new_params, params, optimizer)
-            num_pts = params['means3D'].shape[0]
-
-            variables['means2D_gradient_accum'] = torch.zeros(num_pts, device="cuda")
-            variables['denom'] = torch.zeros(num_pts, device="cuda")
-            variables['max_2D_radius'] = torch.zeros(num_pts, device="cuda")
-            to_remove = torch.cat((to_split, torch.zeros(n * to_split.sum(), dtype=torch.bool, device="cuda")))
-            params, variables = remove_points(to_remove, params, variables, optimizer)
-
-            remove_threshold = 0.25 if i == 5000 else 0.005
-            to_remove = (torch.sigmoid(params['logit_opacities']) < remove_threshold).squeeze()
-            if i >= 3000:
-                big_points_ws = torch.exp(params['log_scales']).max(dim=1).values > 0.1 * variables['scene_radius']
-                to_remove = torch.logical_or(to_remove, big_points_ws)
-            params, variables = remove_points(to_remove, params, variables, optimizer)
-
-            torch.cuda.empty_cache()
-
-        if i > 0 and i % 3000 == 0:
-            new_params = {'logit_opacities': inverse_sigmoid(torch.ones_like(params['logit_opacities']) * 0.01)}
-            params = update_params_and_optimizer(new_params, params, optimizer)
-
-    return params, variables
-
-def save_ply(points, output_filename):
+def fetchPly(path):
     """
-    Save 3D points to a PLY file.
+    Load the point clouds from path, use seg_colors is None
     """
-    points = points.detach().cpu().numpy()
-    # Create a structured numpy array for the vertices
-    vertices = np.zeros(points.shape[0], dtype=[
-        ('x', 'f4'), 
-        ('y', 'f4'), 
-        ('z', 'f4')
-    ])
-    
-    # Populate the vertices array
-    vertices['x'] = points[:, 0]
-    vertices['y'] = points[:, 1]
-    vertices['z'] = points[:, 2]
-    
-    # Create a PlyElement from the vertices
-    ply_element = PlyElement.describe(vertices, 'vertex')
-    
-    # Write the PLY file
-    PlyData([ply_element], text=True).write(output_filename)
-    
-    print(f"Saved {points.shape[0]} points to {output_filename}")
-    
-    
-def visualize_timestep(dataset, t, file_path):
-    """
-    Visualize all gt images for the given dataset.
-    """
-    for camera in dataset: 
-        ind = camera["id"]
-        extension = f"t{t}_{ind}.png" 
-        output_path = os.path.join(file_path, extension)
-        plt.imsave(output_path, camera["im"].permute(1,2,0).detach().cpu().numpy())
-    plt.close()
-    
-
-
-def load_from_checkpoint(checkpoint_path):
-    """
-    Loading parameters, variables, optimizer from checkpoint.
-    """
-    checkpoint = torch.load(checkpoint_path, weights_only=False)
-    params = checkpoint["params"]
-    variables = checkpoint["variables"]
-    
-    return params, variables
+    plydata = PlyData.read(path)
+    vertices = plydata['vertex']
+    positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
+    colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
+    normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    return BasicPointCloud(points=positions, colors=colors, normals=normals, seg_colors=None)
